1b1d418c9e9b830fedabece44095b6ffd85f51af
arvz <arvin.g@gmail.com>
2018-01-26 23:55:20 +1100

    Basic temp Player Controls working. Basic Camera Controller working.

---
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+   [SerializeField] private float _followSpeed = 2f;
+   [SerializeField] private Transform _target;
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   void LateUpdate()
+   {
+      Vector3 newPosition = _target.position;
+      newPosition.z = -10;
+      transform.position = Vector3.Slerp(transform.position, newPosition, _followSpeed * Time.deltaTime);
+   }
+}
--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   [SerializeField] float _speed;
 
    private void Update()
    {
-      
+      float h = Input.GetAxis("Horizontal");
+      float v = Input.GetAxis("Vertical");
+
+      transform.Translate(Vector3.right * h * _speed * Time.deltaTime);
+      transform.Translate(Vector3.up * v * _speed * Time.deltaTime);
+
    }
 
    public void Jump()
