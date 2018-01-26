39e301b1fa3e24e3161cf3df311d2f54100729b7
arvz <arvin.g@gmail.com>
2018-01-27 00:23:06 +1100

    Camera bounds working

--- Assets/_Scripts/Controllers/CameraController.cs
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
    [SerializeField] private float _followSpeed = 2f;
+   [SerializeField] private float _minZoom;
+   [SerializeField] private float _maxZoom;
    [SerializeField] private Transform _target;
 
-   // Use this for initialization
+   [SerializeField] private BoxCollider2D _levelBounds;
+   private Vector3 _min;
+   private Vector3 _max;
+
    void Start()
    {
-
+      _min = _levelBounds.bounds.min;
+      _max = _levelBounds.bounds.max;
    }
 
    void LateUpdate()
@@ -18,5 +24,18 @@ public class CameraController : MonoBehaviour
       Vector3 newPosition = _target.position;
       newPosition.z = -10;
       transform.position = Vector3.Slerp(transform.position, newPosition, _followSpeed * Time.deltaTime);
+
+      var cameraHalfWidth = Camera.main.orthographicSize * ((float)Screen.width / Screen.height);
+      var x = transform.position.x;
+      var y = transform.position.y;
+
+      // lock the camera to the right or left bound if we are touching it
+      x = Mathf.Clamp(x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+
+      // lock the camera to the top or bottom bound if we are touching it
+      y = Mathf.Clamp(y, _min.y + Camera.main.orthographicSize, _max.y - Camera.main.orthographicSize);
+
+      transform.position = new Vector3(x, y, transform.position.z);
+
    }
 }
