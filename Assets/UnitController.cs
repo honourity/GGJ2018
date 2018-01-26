9445c6262011669154eaa99be4adc14a39aa48ff
arvz <arvin.g@gmail.com>
2018-01-27 03:47:34 +1100

    Started implementing unit with temp assets.

---
+++ Assets/UnitController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UnitController : MonoBehaviour
+{
+   public float MaxHealth = 2;
+   public float Health = 2;
+
+   [SerializeField] private Image _healthBar;
+   [SerializeField] private float _desiredDistance = 2.5f;
+   [SerializeField] float _speed = 2f;
+
+   [SerializeField] Transform targetTest;
+
+   private float _initialScale;
+
+   void Start()
+   {
+      _initialScale = transform.localScale.x;
+   }
+
+   void Update()
+   {
+      Vector3 playerPos = GameManager.Instance.PlayerController.transform.position;
+
+      if (Vector2.Distance(transform.position, playerPos) > _desiredDistance)
+      {
+         //Move in closer to the player
+         Vector3 diffVector = playerPos - transform.position;
+
+         if (diffVector.x <= 0)
+            transform.localScale = new Vector3(-_initialScale, _initialScale);
+         else
+            transform.localScale = new Vector3(_initialScale, _initialScale);
+
+         transform.Translate((diffVector) * _speed * Time.deltaTime);
+
+      }
+      else
+      {
+         //Close enough to the player
+      }
+
+   }
+
+   public void TakeDamage(float amount)
+   {
+      Health -= amount;
+   }
+
+}
