3560dd329f19e48eb9c346d9a233988ee6f025a2
Thomas Brown <github@honourity.com>
2018-01-26 23:05:38 +1100

    added Inputmanager and added playerController to game

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+   internal void Jump()
+   {
+      throw new NotImplementedException();
+   }
 }
---
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class InputManager : MonoBehaviour
+{
+   private static InputManager _instance;
+   public static InputManager Instance { get { return _instance = _instance ?? FindObjectOfType<InputManager>(); } }
+
+   private PlayerController _playerController;
+
+   private void Awake()
+   {
+      _playerController = FindObjectOfType<PlayerController>();
+   }
+
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space)) _playerController.Jump();
+   }
+}
