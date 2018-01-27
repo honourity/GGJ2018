a2bf0cebfaf3c9c57fe45614a87bdab958f90f85
arvz <arvin.g@gmail.com>
2018-01-28 02:00:01 +1100

    Main menu basics done. Tower repair sign sprite

--- Assets/_Scripts/MainMenuManager.cs
+++ Assets/_Scripts/MainMenuManager.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour {
+public class MainMenuManager : MonoBehaviour
+{
+   public void StartGame()
+   {
+      SceneManager.LoadScene("Game");
+   }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
