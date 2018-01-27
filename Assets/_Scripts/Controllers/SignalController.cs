9da4faff24418ab1cbc11577f871f2f2ad799d16
Thomas Brown <github@honourity.com>
2018-01-27 22:47:02 +1100

    added signal animation prepped for 8-way

--- Assets/_Scripts/Controllers/SignalController.cs
+++ Assets/_Scripts/Controllers/SignalController.cs
@@ -7,6 +7,7 @@ public class SignalController : MonoBehaviour
    private float _desiredDistance = 0.01f;
    private float _speed = 2f;
    private Vector3 _diffVector;
+   private Animator _animator;
 
    public void Initialize(Vector3 start, Transform end, IMessageReceiver receiver)
    {
@@ -14,7 +15,12 @@ public class SignalController : MonoBehaviour
       _target = end.transform;
       _messageReceiver = receiver;
       _diffVector = _target.position - transform.position;
-      transform.right = -_diffVector;
+      //transform.right = -_diffVector;
+   }
+
+   private void Awake()
+   {
+      _animator = GetComponent<Animator>();
    }
 
    private void Update()
@@ -22,11 +28,25 @@ public class SignalController : MonoBehaviour
       if (Vector2.Distance(transform.position, _target.transform.position) > _desiredDistance)
       {
          transform.Translate((_diffVector.normalized) * _speed * Time.deltaTime, Space.World);
+
+         SetDirection();
       }
       else
       {
          _messageReceiver.ProcessMessage();
          Destroy(gameObject);
       }
+   }
+
+   private void SetDirection()
+   {
+      var adjustedAngle = Vector3.SignedAngle(Vector3.up, _diffVector, Vector3.back);
+      if (adjustedAngle < 0) adjustedAngle += 360f;
+
+      //force only 4-way since the way the Enums.Directions is configured, we cant move around cardinals in sequential order
+      var directionValue = Mathf.RoundToInt(adjustedAngle / 45f);
+      if (directionValue == 8) directionValue = 0;
+      var direction = (Enums.Directions8Way)directionValue;
+      _animator.SetFloat("direction", (int)direction);
    }
 }
