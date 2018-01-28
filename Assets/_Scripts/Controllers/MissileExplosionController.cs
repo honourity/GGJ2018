e64178e658c7155d80714837e5a53ff9db76464c
Thomas Brown <github@honourity.com>
2018-01-28 12:55:31 +1100

    fixed missile explosion sound

--- Assets/_Scripts/Controllers/MissileExplosionController.cs
+++ Assets/_Scripts/Controllers/MissileExplosionController.cs
@@ -2,19 +2,8 @@
 
 public class MissileExplosionController : MonoBehaviour
 {
-   [SerializeField]
-   private AudioClip _explosionSound = null;
-
-   private AudioSource _audioSource;
-
-   private void Awake()
-   {
-      _audioSource = GetComponent<AudioSource>();
-   }
-
    private void Start()
    {
-      _audioSource.PlayOneShot(_explosionSound);
-      Destroy(gameObject, 0.5f);
+      Destroy(gameObject, 2f);
    }
 }
