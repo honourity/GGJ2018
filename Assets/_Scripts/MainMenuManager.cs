06a91f983e32633e1a642898752b86d79c8718d9
arvz <arvin.g@gmail.com>
2018-01-28 11:36:16 +1100

    Finished Main Menu stuff. Added max health BG and max ultimate charge BG. Deleted temp assets

--- Assets/_Scripts/MainMenuManager.cs
+++ Assets/_Scripts/MainMenuManager.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+   private Animator _animator;
+
+   private void Awake()
+   {
+      _animator = GetComponent<Animator>();
+   }
+
+   public void PlayButton()
+   {
+      Debug.Log("Start Game!");
+      _animator.Play("StartGameFade");
+   }
+
    public void StartGame()
    {
       SceneManager.LoadScene("Game");
    }
-
 }
